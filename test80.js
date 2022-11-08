ce77f2a0645cff1604eccb0368d6f7da83691418
canselsenyurt <senyurtcansel@gmail.com>
2022-11-08 17:43:16 +0300

    first commit

---
+++ test80.js
@@ -0,0 +1,9 @@
+var isim = prompt("Merhabalar Lutfen isminizi giriniz");
+alert("Hosgeldiniz " + isim );
+var dogumtarihi = prompt("Merhaba Lutfen Dogum Tarihinizi giriniz");
+var yas;
+yas = 2022 - dogumtarihi;
+
+
+alert("Merhaba " + isim+ ", " + yas + " yasindasiniz.");
+console.log(isim + yas);// JavaScript Document
